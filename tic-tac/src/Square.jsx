70163c056daad0a6266f95dcb6b5d56eb34f615c
Sonikesh <91758635+SonikeshKumar14@users.noreply.github.com>
2022-08-30 21:48:24 +0530

    Add files via upload

---
+++ tic-tac/src/Square.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+const Square = (props) =>{
+    return(
+        <div style={{
+         border: "1px solid",
+         height: '100px', 
+         width: '100%',
+         display: 'flex',
+         alignItems: 'center',
+         justifyContent: 'center',
+         }} className='square'
+         onClick={props.onClick}>
+            <h5>{props.value}</h5>
+        </div>
+    )
+}
+
+export default Square;
